9f0521b35ad9c0bd42bcf4e0b5aa42d654578adf
alonso113 <geesinkantonalonso@gmail.com>
2025-10-09 09:40:43 +0200

    fix

--- tls_http_response.go
+++ tls_http_response.go
@@ -30,8 +30,6 @@ func (r *tlsHttpResponse) Body() ([]byte, error) {
 	}()
 	b, err := io.ReadAll(r.response.Body)
 	return b, err
-	defer r.response.Body.Close()
-	return io.ReadAll(r.response.Body)
 }
 
 func (r *tlsHttpResponse) Header(str string) string {
